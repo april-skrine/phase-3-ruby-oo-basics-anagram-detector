35982bf579025c51404a10513e9adb568040b63e
April Skrine <april.skrine@gmail.com>
2022-02-23 16:09:44 -0600

    commit tree 2fb2e20652a81951599e5c30ec063d165ee8cdae

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    # MATCH
+    # Takes an array of words
+    # Seperates those words into characters
+    # Seperates the intialize word into characters
+    # Compares each list of characters to the intialize word chars
+    # Maybe they need to be in the same order?
+
+    def match(some_array)
+        words_that_match = []
+        some_array.each do |word|
+            if word.chars.sort == @word.chars.sort
+              words_that_match << word
+            end  
+        end
+        return words_that_match
+    end
+
+end
